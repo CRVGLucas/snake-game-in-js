4a6616312b59bb5095777a642b390538a4c78adb
Lucas <lucas@techdog.com.br>
2021-12-22 09:36:32 -0300

    definindo direcoes

--- index.js
+++ index.js
@@ -11,18 +11,55 @@ cobra[0] = {
 var direction = 'right'
 
 function criarBackground() {
-    context.fillRect( 0 , 0, 16 * box, 16 * box )
     context.fillStyle = 'lightgreen'
+    context.fillRect( 0 , 0, 16 * box, 16 * box )
 }
 
 function criarCobra() {
     for( i = 0; i < snake.length; i++ ) {
-        context.fillRect(snake[i].x, snake[i].y, box, box)
         context.fillStyle = 'green'
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+document.addEventListener('keydown', update)
+
+function update( event ) {
+    if( event.keyCode == 37 && direction !== 'right') {
+        direction = 'left'
+    }
+
+    if( event.keyCode == 38 && direction !== 'down') {
+        direction = 'up'
+    }
+
+    if( event.keyCode == 39 && direction !== 'left') {
+        direction = 'right'
+    } 
+
+    if( event.keyCode == 40 && direction !== 'up') {
+        direction = 'down'
     }
 }
 
 function iniciarJogo() {
+    
+    if( cobra[0].x > 15 * box && direction == 'right') {
+        cobra[0].x = 0
+    }
+
+    if( cobra[0].x < 0 && direction == 'left') {
+        cobra[0].x = 16 * box
+    }
+
+    if( cobra[0].y > 15 * box && direction == 'down' ) {
+        cobra[0].y = 0
+    }
+
+    if( cobra[0].y < 0 && direction == 'up' ) {
+        cobra[0].y = 16 * box
+    }
+
     criarBackground()
     criarCobra()
 
@@ -32,12 +69,15 @@ function iniciarJogo() {
     if( direction == 'right' ) {
         cobraX += box
     } 
+
     if ( direction == 'left' ) {
         cobraX -= box
     }
+
     if( direction == 'up' ) {
         cobraY -= box
     }
+
     if( direction == 'down' ) {
         cobraY += box
     }
@@ -52,4 +92,5 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100)
+jogo
 
